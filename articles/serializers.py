81cd7dd7e841ca37e6bd92a9c51c4fe148d5c83e
USER\HOME <leekh9997@naver.com>
2023-09-25 20:29:39 +0900

    article create

---
+++ articles/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from articles.models import Article
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ("title", "image", "content")
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    def get_user(self, obj):
+        return obj.user.email
+
+    class Meta:
+        model = Article
+        fields = ("pk", "title", "image", "updated_at",
+                  "user")
--- articles/views.py
+++ articles/views.py
@@ -4,15 +4,24 @@ from rest_framework import status, permissions
 from rest_framework.response import Response
 
 from articles.models import Article
+from articles.serializers import ArticleCreateSerializer, ArticleListSerializer
+
 
 
 # 첫화면
 class ArticleView(APIView):
     def get(self, request):
-        pass
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        pass
+        serializer = ArticleCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 모아보기(feed)
