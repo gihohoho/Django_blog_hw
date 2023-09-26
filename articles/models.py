c0f4e1802740c8bd334d3884b08b7839fb83357f
USER\HOME <leekh9997@naver.com>
2023-09-26 10:56:32 +0900

    comment model

--- articles/models.py
+++ articles/models.py
@@ -13,3 +13,15 @@ class Article(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.content)
